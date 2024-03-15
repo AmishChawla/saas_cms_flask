87f76b44add1a1dd98028188d52daef9735cb309
pokhrajshirsat <pok.shi.rt20@dypatil.edu>
2024-03-15 16:52:52 +0530

    plan display added

--- app.py
+++ app.py
@@ -802,6 +802,12 @@ def delete_plan(plan_id):
         return redirect(url_for('list_of_plans'))
 
 
+@app.route("/plans", methods=['GET', 'POST'])
+@login_required
+def user_view_plan():
+    result = api_calls.get_all_plans()
+    return render_template('user_view_plan.html', result=result)
+
 
 
 if __name__ == '__main__':
