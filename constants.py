10ead24742a1e4cdba172cb52f2e4e17a91cd4b6
pokhrajshirsat <pok.shi.rt20@dypatil.edu>
2024-05-14 12:44:07 +0530

    CMS added in user dashboard

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 import os
 
-BASE_URL = 'http://127.0.0.1:8000/api'
+# BASE_URL = 'http://127.0.0.1:8000/api'
+#
+#
+# ROOT_URL = 'http://127.0.0.1:8000'
 
 
-ROOT_URL = 'http://127.0.0.1:8000'
-
-
-# ROOT_URL = 'http://35.154.190.245:8000'
-# BASE_URL = 'http://35.154.190.245:8000/api'
+ROOT_URL = 'http://35.154.190.245:8000'
+BASE_URL = 'http://35.154.190.245:8000/api'
 
 from flask.cli import load_dotenv
 
