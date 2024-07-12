4fbc5b4f045a6aa0ad01f4baf861f07bb7cfa1dd
pokhrajshirsat <pok.shi.rt20@dypatil.edu>
2024-07-12 17:53:41 +0530

    like and reply issue solved

--- constants.py
+++ constants.py
@@ -3,17 +3,17 @@ from flask.cli import load_dotenv
 
 
 # MY ROOT URL
-MY_ROOT_URL = 'http://127.0.0.1:5000'
-# MY_ROOT_URL = 'http://35.154.190.245:5000'
+# MY_ROOT_URL = 'http://127.0.0.1:5000'
+MY_ROOT_URL = 'http://35.154.190.245:5000'
 
 
 
 # FOR API
-BASE_URL = 'http://127.0.0.1:8000/api'
-ROOT_URL = 'http://127.0.0.1:8000'
+# BASE_URL = 'http://127.0.0.1:8000/api'
+# ROOT_URL = 'http://127.0.0.1:8000'
 
-# ROOT_URL = 'http://35.154.190.245:8000'
-# BASE_URL = 'http://35.154.190.245:8000/api'
+ROOT_URL = 'http://35.154.190.245:8000'
+BASE_URL = 'http://35.154.190.245:8000/api'
 
 
 
