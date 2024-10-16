0e9a0d0e6e271827bf9e08a52ac9f53e98c2c28c
pokhrajshirsat <pok.shi.rt20@dypatil.edu>
2024-10-16 14:58:11 +0530

    username error fixed

--- constants.py
+++ constants.py
@@ -3,11 +3,11 @@ from flask.cli import load_dotenv
 
 
 # FOR API
-# BASE_URL = 'http://127.0.0.1:8000/api'
-# ROOT_URL = 'http://127.0.0.1:8000'
+BASE_URL = 'http://127.0.0.1:8000/api'
+ROOT_URL = 'http://127.0.0.1:8000'
 
-ROOT_URL = 'http://35.154.190.245:8000'
-BASE_URL = 'http://35.154.190.245:8000/api'
+# ROOT_URL = 'http://35.154.190.245:8000'
+# BASE_URL = 'http://35.154.190.245:8000/api'
 
 
 load_dotenv('secrets.env')
